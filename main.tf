0779cda8b82cda1a65f54b62dbeab9aa78e4e959
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-02 13:03:13 -0300

    Add new resource group

--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ resource "azurerm_resource_group" "rg-test-tfstate" {
   name     = var.rg-name
   tags     = { Departamento = "Tecnologia da Informação" }
 }
+
+resource "azurerm_resource_group" "rg-via-pipeline" {
+  location = "eastus"
+  name     = "rg-via-pipeline"
+  tags     = { Departamento = "Tecnologia da Informação" }
+}
